c8403f66b8667aa0fd824d66e9b02a9401a3cb9e
man-b-e-a-s-t <jason.ray89@gmail.com>
2026-05-09 11:37:13 -0400

    chore: gems and schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -36,11 +36,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_07_120000) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "email", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
